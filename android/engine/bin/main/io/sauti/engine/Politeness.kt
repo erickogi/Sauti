25510a75aceb55d6f56627e869b33f01a9e95e44
kogicodes <kogicodes@gmail.com>
2026-07-28 04:44:20 +0300

    Stop tracking android/engine/bin build output

--- android/engine/bin/main/io/sauti/engine/Politeness.kt
+++
@@ -1,7 +0,0 @@
-package io.sauti.engine
-
-object Politeness {
-    fun politePeer(a: String, b: String): String = if (a <= b) a else b
-
-    fun isPolite(selfId: String, peerId: String): Boolean = politePeer(selfId, peerId) == selfId
-}
